9c8cab7846e17ec38b480828890217699ac7829c
riad <riadmefti@hotmail.com>
2022-01-20 14:50:07 -0500

    ajout du component footer et button

---
+++ client/src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+
+  @Input()  name:string;
+  @Input()  routing:string;
+  constructor() { 
+  
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
