2a52e9d048bdc28b3f80c4fc111953f809447b8a
Ian Alexis <you@example.com>
2022-01-22 19:00:33 -0500

    chevalet

---
+++ client/src/app/components/tile-holder/tile-holder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tile-holder',
+  templateUrl: './tile-holder.component.html',
+  styleUrls: ['./tile-holder.component.scss']
+})
+export class TileHolderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
