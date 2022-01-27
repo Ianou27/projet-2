f9b2545f99ad9dcb810cd8174363305b811396b7
Blinkod <71103018+Blinkod@users.noreply.github.com>
2022-01-26 19:46:09 -0500

    Validation des mots fonctionne avec Tests

--- server/app/classes/game.spec.ts
+++ server/app/classes/game.spec.ts
@@ -4,12 +4,17 @@ import { Game } from './game';
 describe('GameClass', () => {
     const game: Game = new Game();
     it('method validatedWord should return True when a word is in the dictionary', () => {
-        let result = game.validatedWord("aas");
+        let result = game.validatedWord("ourson");
         expect(result).to.equals(true);
     });
 
-    it('method validatedWord should return False when a word is in the dictionary', () => {
-        let result = game.validatedWord("abbfds");
+    it('method validatedWord should return False when a word is not in the dictionary', () => {
+        let result = game.validatedWord("aaaaaaa");
+        expect(result).to.equals(false);
+    });
+
+    it('method validatedWord should return False when a word is smaller than 2 letters', () => {
+        let result = game.validatedWord("a");
         expect(result).to.equals(false);
     });
 });
--- server/app/classes/game.ts
+++ server/app/classes/game.ts
@@ -1,48 +1,48 @@
 declare var require: any
-
+const fs = require('fs');
 
 export class Game  {
     turnPlayerOne: boolean;
     dictionary: string;
-    dict: any;
 
     constructor(){
         this.turnPlayerOne = true;
         this.dictionary = "Mon dictionnaire";
-        const fs = require('fs');
-        const path = require('path');
-        this.dict = fs.readFileSync(path.resolve('./dictionnary.json'));
+
     }
 
-    changeTurn(){
+/*     changeTurn(){
         this.turnPlayerOne = !this.turnPlayerOne;
-    }
+    } */
 
-    //To be implemented
-    //changeDictionary(){}
+/*     To be implemented
+    changeDictionary(){} */
 
     validatedWord(word:string) : boolean {
         if(word.length < 2)
             return false;
 
-        let dictionaryArray: string | string[] = ["aa","aalenien","aalenienne","aaleniennes","aaleniens","aas","abaca","abacas","abacost","abacosts","abacule","abacules","abaissa","abaissable","abaissables","abaissai","abaissaient","abaissais","abaissait","abaissames","abaissant","abaissante","abaissantes","abaissants","abaissas","abaissasse","abaissassent","abaissasses","abaissassiez","abaissassions","abaissat","abaissates","abaisse","abaissee","abaissees","abaissement","abaissements","abaissent","abaisser","abaissera","abaisserai","abaisseraient","abaisserais","abaisserait","abaisseras","abaisserent","abaisserez","abaisseriez","abaisserions","abaisserons","abaisseront","abaisses","abaisseur","abaisseurs","abaisseuse","abaisseuses","abaissez","abaissiez","abaissions","abaissons","abajoue","abajoues","abale","abales","abalone","abalones","abandon","abandonna","abandonnai","abandonnaient","abandonnais","abandonnait","abandonnames","abandonnant","abandonnas","abandonnasse","abandonnassent","abandonnasses","abandonnassiez","abandonnassions","abandonnat","abandonnataire","abandonnataires","abandonnates","abandonnateur","abandonnateurs","abandonnatrice","abandonnatrices","abandonne","abandonnee","abandonnees","abandonnement","abandonnements","abandonnent","abandonner","abandonnera","abandonnerai","abandonneraient","abandonnerais","abandonnerait","abandonneras","abandonnerent","abandonnerez","abandonneriez","abandonnerions","abandonnerons","abandonneront","abandonnes","abandonnez","abandonniez","abandonnions","abandonnique","abandonniques"];
+        let dictionaryArray: string[] = JSON.parse(fs.readFileSync("./assets/dictionnary.json")).words;
         let leftLimit = 0;
         let rightLimit = dictionaryArray.length - 1;
-
         while (leftLimit <= rightLimit) {
             let middleLimit = leftLimit + Math.floor((rightLimit - leftLimit) / 2);
             
             //localeCompare helps us know if the word is before(-1), equivalent(0) or after(1)
             let comparisonResult = word.localeCompare(dictionaryArray[middleLimit]);
-                
-            if (comparisonResult < 0)
+
+            if (comparisonResult < 0) {
                 rightLimit = middleLimit - 1;
-
-            if (comparisonResult > 0)
+            }
+                
+            else if (comparisonResult > 0) {
                 leftLimit = middleLimit + 1;
+            }
 
-            else
+            else {
                 return true;
+            }
+                
         }
         return false;
     }
