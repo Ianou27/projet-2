cb318a0dcdbef65f9a6aaab4e1ab121e55cc5a71
ianou27 <alexis.ian23@gmail.com>
2022-01-27 11:19:13 -0500

    ajout d'une interface tuile et data binding dans le html de tuile

--- client/src/app/components/tile/tile.component.ts
+++ client/src/app/components/tile/tile.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Tile } from '@app/classes/tile';
 
 @Component({
-  selector: 'app-tile',
-  templateUrl: './tile.component.html',
-  styleUrls: ['./tile.component.scss']
+    selector: 'app-tile',
+    templateUrl: './tile.component.html',
+    styleUrls: ['./tile.component.scss'],
 })
 export class TileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    tile: Tile = { letter: 'e', value: 1 };
+    constructor() {}
+    ngOnInit(): void {}
 }
