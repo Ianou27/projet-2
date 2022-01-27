963dadd252919e1f6477c4a23a035ba3fa152291
Blinkod <71103018+Blinkod@users.noreply.github.com>
2022-01-27 12:34:06 -0500

    Changement nom fichier playerService

---
+++ client/src/app/classes/playerService/playerService.ts
@@ -0,0 +1,25 @@
+enum playerTypeEnum{
+    Bot,
+    Human,
+}
+
+export class PlayerService{
+    playerType: playerTypeEnum;
+    letters: Array<String>;
+    hisTurn: boolean;
+
+    constructor(playerType:playerTypeEnum, letters:Array<String>, hisTurn:boolean){
+        this.playerType = playerType;
+        this.letters = letters;
+        this.hisTurn = hisTurn; 
+    }
+
+    changeTurn(){
+        this.hisTurn = !this.hisTurn;
+    }
+
+    validatedWord(word:string){
+        //todo verify if the 
+    }
+
+}
