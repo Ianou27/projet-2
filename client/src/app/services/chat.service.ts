8542f5a64f294c85a67404c985f2b6ba85d08366
RichardT15 <richard.tham@polymtl.ca>
2022-01-29 13:09:40 -0500

    RT: ajout du message d'erreur lorsque le message est trop long

--- client/src/app/services/chat.service.ts
+++ client/src/app/services/chat.service.ts
@@ -1,97 +1,95 @@
 import { Injectable } from '@angular/core';
 import { SocketClientService } from './socket-client.service';
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ChatService {
+    serverMessage: string = '';
+    serverClock: Date;
 
- serverMessage: string = "";
-  serverClock: Date;
+    wordInput = '';
+    serverValidationResult = '';
+    messageToServer = '';
 
-  wordInput = "";
-  serverValidationResult = "";
-  messageToServer = "";
+    broadcastMessage = '';
+    serverMessages: string[] = [];
 
-  broadcastMessage = "";
-  serverMessages: string[] = [];
+    roomMessage = '';
+    roomMessages: string[] = [];
 
-  roomMessage = "";
-  roomMessages: string[] = [];
+    constructor(public socketService: SocketClientService) {}
 
-  constructor(public socketService: SocketClientService) { }
-
-  get socketId() {
-    return this.socketService.socket.id ? this.socketService.socket.id : "";
-  }
-
-  ngOnInit(): void {
-    this.connect();
-  }
-
-  connect() {
-    if (!this.socketService.isSocketAlive()) {
-      this.socketService.connect();
-      this.configureBaseSocketFeatures();
+    get socketId() {
+        return this.socketService.socket.id ? this.socketService.socket.id : '';
     }
-  }
 
+    ngOnInit(): void {
+        this.connect();
+    }
 
-  configureBaseSocketFeatures() {
-    this.socketService.on("connect", () => {
-      console.log(`Connexion par WebSocket sur le socket ${this.socketId}`);
-    });
-    // Afficher le message envoyé lors de la connexion avec le serveur
-    this.socketService.on("hello", (message: string) => {
-      this.serverMessage = message;
-    });
+    connect() {
+        if (!this.socketService.isSocketAlive()) {
+            this.socketService.connect();
+            this.configureBaseSocketFeatures();
+        }
+    }
 
-    // Afficher le message envoyé à chaque émission de l'événement "clock" du serveur
-    this.socketService.on("clock", (time: Date) => {
-      this.serverClock = time;
-    });
+    configureBaseSocketFeatures() {
+        this.socketService.on('connect', () => {
+            console.log(`Connexion par WebSocket sur le socket ${this.socketId}`);
+        });
+        // Afficher le message envoyé lors de la connexion avec le serveur
+        this.socketService.on('hello', (message: string) => {
+            this.serverMessage = message;
+        });
 
-    // Gérer l'événement envoyé par le serveur : afficher le résultat de validation
-    this.socketService.on('wordValidated', (isValid: boolean) => {
-      isValid ?  this.broadcastMessageToAll():'dfsfsdf';
-     
-    });
+        // Afficher le message envoyé à chaque émission de l'événement "clock" du serveur
+        this.socketService.on('clock', (time: Date) => {
+            this.serverClock = time;
+        });
 
-    // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un client connecté
-    this.socketService.on('massMessage', (broadcastMessage: string) => {
-      this.serverMessages.push(broadcastMessage);
-    });
+        // Gérer l'événement envoyé par le serveur : afficher le résultat de validation
+        this.socketService.on('wordValidated', (isValid: boolean) => {
+            isValid ? this.broadcastMessageToAll() : this.messageLengthError();
+        });
 
-    // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un membre de la salle
-    this.socketService.on('roomMessage', (roomMessage: string) => {
-      this.roomMessages.push(roomMessage);
-    });
-  }
+        // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un client connecté
+        this.socketService.on('massMessage', (broadcastMessage: string) => {
+            this.serverMessages.push(broadcastMessage);
+        });
 
-  sendWordValidation() {
-    this.socketService.send('validate', this.broadcastMessage);
-    
-  }
+        // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un membre de la salle
+        this.socketService.on('roomMessage', (roomMessage: string) => {
+            this.roomMessages.push(roomMessage);
+        });
+    }
 
-  sendToServer() {
-    this.socketService.send('message', this.messageToServer);
-    this.messageToServer = "";
-  }
+    sendWordValidation() {
+        this.socketService.send('validate', this.broadcastMessage);
+    }
 
-  broadcastMessageToAll() {
-    this.socketService.send('broadcastAll', this.broadcastMessage);
-    this.broadcastMessage = "";
-  }
+    sendToServer() {
+        this.socketService.send('message', this.messageToServer);
+        this.messageToServer = '';
+    }
 
-  joinRoom() {
-    this.socketService.send("joinRoom");
-  }
+    broadcastMessageToAll() {
+        this.socketService.send('broadcastAll', this.broadcastMessage);
+        this.broadcastMessage = '';
+    }
 
-  sendToRoom() {
-    this.socketService.send('roomMessage', this.roomMessage);
-    this.roomMessage = "";
-  }
-  
+    joinRoom() {
+        this.socketService.send('joinRoom');
+    }
 
+    sendToRoom() {
+        this.socketService.send('roomMessage', this.roomMessage);
+        this.roomMessage = '';
+    }
+
+    messageLengthError() {
+        this.serverMessages.push('Votre message est trop long');
+        this.broadcastMessage = '';
+    }
 }
