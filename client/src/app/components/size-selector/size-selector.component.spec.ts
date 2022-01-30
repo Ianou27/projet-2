02c43a5f5904a4e517cdea4c735a490d963761b6
Ian Alexis <you@example.com>
2022-01-30 11:15:23 -0500

    ajout d'un test pour le size selector

--- client/src/app/components/size-selector/size-selector.component.spec.ts
+++ client/src/app/components/size-selector/size-selector.component.spec.ts
@@ -1,13 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ResizerService } from '@app/services/resizer.service';
 import { SizeSelectorComponent } from './size-selector.component';
+import SpyObj = jasmine.SpyObj;
 
 describe('SizeSelectorComponent', () => {
+    let resizerServiceSpy: SpyObj<ResizerService>;
     let component: SizeSelectorComponent;
     let fixture: ComponentFixture<SizeSelectorComponent>;
+
+    beforeEach(() => {
+        resizerServiceSpy = jasmine.createSpyObj('ResizerService', ['changeFont']);
+    });
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [SizeSelectorComponent],
+            providers: [{ provide: ResizerService, useValue: resizerServiceSpy }],
         }).compileComponents();
     });
 
@@ -19,5 +27,10 @@ describe('SizeSelectorComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('changeFont should call changeFont in the service', () => {
+        component.changeFont('-');
+        expect(resizerServiceSpy.changeFont).toHaveBeenCalled();
     });
 });
