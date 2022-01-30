cc0045ab5695ea0d998927cb483f1af1654959df
RichardT15 <richard.tham@polymtl.ca>
2022-01-30 13:22:43 -0500

    RT:création de fnc vérification commande (à implémenter)

--- server/app/services/socketManager.service.ts
+++ server/app/services/socketManager.service.ts
@@ -1,33 +1,31 @@
-import * as io from 'socket.io';
 import * as http from 'http';
-
+import * as io from 'socket.io';
 
 export class SocketManager {
-
     private sio: io.Server;
-    private room: string = "serverRoom";
+    private room: string = 'serverRoom';
     constructor(server: http.Server) {
-        this.sio = new io.Server(server, { cors: { origin: '*', methods: ["GET", "POST"] } });
+        this.sio = new io.Server(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
     }
 
-    public handleSockets(): void {
+    handleSockets(): void {
         this.sio.on('connection', (socket) => {
-            console.log(`Connexion par l'utilisateur avec id : ${socket.id}`)
+            console.log(`Connexion par l'utilisateur avec id : ${socket.id}`);
             // message initial
-            socket.emit("hello", "Hello World!");
+            socket.emit('hello', 'Hello World!');
 
             socket.on('message', (message: string) => {
                 console.log(message);
             });
             socket.on('validate', (message: string) => {
-
+                // const isValid = message[0] === '!' ? this.commandVerification(message) : this.lengthVerification(message) && this.characterVerification(message);
                 const isValid = this.lenghtVerification(message) && this.characterVerification(message);
                 socket.emit('wordValidated', isValid);
-            })
+            });
 
             socket.on('broadcastAll', (message: string) => {
-                this.sio.sockets.emit("massMessage", `${socket.id} : ${message}`)
-            })
+                this.sio.sockets.emit('massMessage', `${socket.id} : ${message}`);
+            });
 
             socket.on('joinRoom', () => {
                 socket.join(this.room);
@@ -37,17 +35,14 @@ export class SocketManager {
                 const roomSockets = this.sio.sockets.adapter.rooms.get(this.room);
                 // Seulement un membre de la salle peut envoyer un message aux autres
                 if (roomSockets?.has(socket.id)) {
-                    this.sio.to(this.room).emit("roomMessage", `${socket.id} : ${message}`);
+                    this.sio.to(this.room).emit('roomMessage', `${socket.id} : ${message}`);
                 }
             });
 
-
-            socket.on("disconnect", (reason) => {
+            socket.on('disconnect', (reason) => {
                 console.log(`Deconnexion par l'utilisateur avec id : ${socket.id}`);
-                console.log(`Raison de deconnexion : ${reason}`)
+                console.log(`Raison de deconnexion : ${reason}`);
             });
-
-
         });
 
         setInterval(() => {
@@ -58,17 +53,16 @@ export class SocketManager {
     private emitTime() {
         this.sio.sockets.emit('clock', new Date().toLocaleTimeString());
     }
-    private lenghtVerification(message: string){
+    private lenghtVerification(message: string) {
         if (message === undefined) return false;
         if (message.length > 512) return false;
         return true;
-
     }
-    private characterVerification(message: string):boolean{
-        
-        return message.trim().length === 0? false : true; 
-        
+    private characterVerification(message: string): boolean {
+        return message.trim().length === 0 ? false : true;
     }
 
-  
-}
+    /* private commandVerification(message: string): boolean{
+        Verifier si le mot après le point d'exclamation est une commande dans la liste de commande
+    }*/
+    
