ec3ed7fe5b88e4d2026d9e430a5f15bf16a0fe10
Blinkod <71103018+Blinkod@users.noreply.github.com>
2022-02-02 17:45:03 -0500

    Ajout test bouton variable disable

--- client/src/app/components/button/button.component.spec.ts
+++ client/src/app/components/button/button.component.spec.ts
@@ -1,10 +1,12 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
+
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let submitEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +19,22 @@ describe('ButtonComponent', () => {
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    submitEl = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('disable set to true should disabled the button ', () => {
+    component.disable = "true";
+    fixture.detectChanges();
+    expect(submitEl.nativeElement.querySelector('button').disabled).toBeTruthy();
+  });
+
+  it('disable not set to a value should not disabled the button ', () => {
+    fixture.detectChanges();
+    expect(submitEl.nativeElement.querySelector('button').disabled).toBeFalsy();
+  });
+
 });
