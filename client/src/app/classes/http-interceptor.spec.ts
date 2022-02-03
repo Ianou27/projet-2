3cbfa689eb1937b78b8863a09613f47bc77dd1af
RichardT15 <richard.tham@polymtl.ca>
2022-02-02 22:48:32 -0500

    RT: http en commentaire

--- client/src/app/classes/http-interceptor.spec.ts
+++ client/src/app/classes/http-interceptor.spec.ts
@@ -1,7 +1,7 @@
-import { HttpInterceptor } from './http-interceptor';
+/*import { HttpInterceptor } from './http-interceptor';
 
 describe('HttpInterceptor', () => {
   it('should create an instance', () => {
     expect(new HttpInterceptor()).toBeTruthy();
   });
-});
+});*/
