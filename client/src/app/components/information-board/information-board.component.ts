163e9c538a315a36a71bf521e99d5caeac53bfe0
Ian Alexis <you@example.com>
2022-02-03 18:10:40 -0500

    enlever le timer service

--- client/src/app/components/information-board/information-board.component.ts
+++ client/src/app/components/information-board/information-board.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TimerService } from '@app/services/timer.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-information-board',
     templateUrl: './information-board.component.html',
     styleUrls: ['./information-board.component.scss'],
 })
-export class InformationBoardComponent implements OnInit {
-    countDown: Subscription;
-    counter = 60;
-    tick = 1000;
-
-    constructor(private timerService: TimerService) {}
-
-    ngOnInit() {
-        this.countDown = this.timerService.getCounter(this.tick).subscribe(() => {
-            if (this.counter > 0) {
-                this.counter--;
-            }
-        });
-    }
-}
+export class InformationBoardComponent {}
