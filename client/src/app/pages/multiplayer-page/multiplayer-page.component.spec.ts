fb41066181ca6f11f4c5dd6ee3486565e7485bc6
David Ringuet <david.ringuet@polymtl.ca>
2022-02-03 10:49:56 -0500

    DR merge demo1

--- client/src/app/pages/multiplayer-page/multiplayer-page.component.spec.ts
+++ client/src/app/pages/multiplayer-page/multiplayer-page.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MultiplayerPageComponent } from './multiplayer-page.component';
+
+const mockMatDialogRef = {
+    close: () => {
+        return;
+    },
+};
 
 describe('MultiplayerPageComponent', () => {
     let component: MultiplayerPageComponent;
@@ -8,6 +15,16 @@ describe('MultiplayerPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [MultiplayerPageComponent],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: mockMatDialogRef,
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {},
+                },
+            ],
         }).compileComponents();
     });
 
--- client/src/app/pages/scrabble-classic-page/scrabble-classic-page.component.spec.ts
+++ client/src/app/pages/scrabble-classic-page/scrabble-classic-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { ScrabbleClassicPageComponent } from './scrabble-classic-page.component';
 
 describe('ScrabbleClassicPageComponent', () => {
@@ -8,6 +10,7 @@ describe('ScrabbleClassicPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ScrabbleClassicPageComponent],
+            providers: [{ provide: MatDialog, useValue: { open: () => ({ afterClosed: () => new Observable() }) } }],
         }).compileComponents();
     });
 
