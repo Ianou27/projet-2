99372a160e413c14e799bb0221cdd508c73be5e7
riad <riadmefti@hotmail.com>
2022-02-09 16:32:04 -0500

    linking socketJoinRoom and socketCreateRoom to view

--- client/src/app/pages/join-page/join-page.component.ts
+++ client/src/app/pages/join-page/join-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { WaitingPlayerDialogComponent } from '@app/components/waiting-player-dialog/waiting-player-dialog.component';
+import { ChatService } from '@app/services/chat.service';
 
 @Component({
     selector: 'app-join-page',
@@ -9,18 +10,23 @@ import { WaitingPlayerDialogComponent } from '@app/components/waiting-player-dia
     styleUrls: ['./join-page.component.scss'],
 })
 export class JoinPageComponent implements OnInit {
-    name: string;
+    name: string ;
+   
     time: string;
     form: FormGroup;
     alphaNumericRegex = /^[a-zA-Z]*$/;
     selectedDico = 'Dictionnaire par defaut';
     selectedTime = '1';
 
-    constructor(public dialogRef: MatDialogRef<JoinPageComponent>, public waitDialog: MatDialog) {}
+    constructor(public dialogRef: MatDialogRef<JoinPageComponent>, public waitDialog: MatDialog, public chatService:ChatService) {}
     ngOnInit(): void {
         this.form = new FormGroup({
             name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(this.alphaNumericRegex)]),
+   
+
         });
+
+        this.chatService.connect();
     }
     myError = (controlName: string, errorName: string) => {
         // eslint-disable-next-line no-invalid-this
@@ -30,5 +36,13 @@ export class JoinPageComponent implements OnInit {
         this.waitDialog.open(WaitingPlayerDialogComponent, {
             disableClose: true,
         });
+    }
+
+    createRoom(){
+        this.chatService.createRoom(this.name,this.name);
+    }
+
+    joinRoom(){
+        console.log(this.chatService.username);
     }
 }
