0c2d26d7dfe405598f45afcb6971f8ab158b43b9
David Ringuet <david.ringuet@polymtl.ca>
2022-02-10 20:05:36 -0500

    DR correction erreur langue

--- client/src/app/pages/join-page/join-page.component.ts
+++ client/src/app/pages/join-page/join-page.component.ts
@@ -12,18 +12,39 @@ import { ChatService } from '@app/services/chat.service';
 })
 export class JoinPageComponent implements OnInit {
     name: string;
+    confirmName: string;
     time: string;
     form: FormGroup;
+    // usernamePlayer1: string;
     alphaNumericRegex = /^[a-zA-Z]*$/;
     selectedDico = 'Dictionnaire par defaut';
     selectedTime = '1';
+    registerForm: FormGroup;
 
-    constructor(public waitDialog: MatDialog, public chatService: ChatService) {}
-    ngOnInit(): void {
-        this.form = new FormGroup({
-            name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern(this.alphaNumericRegex)]),
-        });
+    constructor(public waitDialog: MatDialog, public chatService: ChatService) {
+        // this.usernamePlayer1 = this.chatService.allRooms[0].player1;
     }
+    ngOnInit(): void {
+        this.form = new FormGroup(
+            {
+                name: new FormControl('', [
+                    Validators.required,
+                    Validators.minLength(3),
+                    Validators.pattern(this.alphaNumericRegex),
+                    // Validators.pattern(this.usernamePlayer1),
+                ]),
+                // confirmName: new FormControl('', [Validators.required]),
+            },
+            // {
+            //     validator: compareName('name', 'confirmName'),
+            // },
+        );
+    }
+
+    get f() {
+        return this.registerForm.controls;
+    }
+
     myError = (controlName: string, errorName: string) => {
         // eslint-disable-next-line no-invalid-this
         return this.form.controls[controlName].hasError(errorName);
@@ -48,4 +69,17 @@ export class JoinPageComponent implements OnInit {
         this.waitDialog.closeAll();
         window.location.href = '/home';
     }
+}
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function compareName(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    };
 }
