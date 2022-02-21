8e725da5792a8826931b40f7c63a12b97ed60311
Ian Alexis <you@example.com>
2022-02-20 20:21:59 -0500

    swapping letters works

--- client/src/app/components/tile-holder/tile-holder.component.ts
+++ client/src/app/components/tile-holder/tile-holder.component.ts
@@ -14,16 +14,26 @@ export class TileHolderComponent {
 
     @HostListener('body:keydown', ['$event'])
     buttonDetect(event: KeyboardEvent) {
+        const swapper = document.getElementById('swap-selected');
         this.buttonPressed = event.key;
-        if (this.buttonPressed === 'ArrowLeft') {
-            this.swap();
+        if (this.buttonPressed === 'ArrowLeft' && swapper) {
+            if (this.isAtExtremity(swapper) === 'Left') {
+                this.endToEnd(swapper);
+            } else {
+                this.swap();
+            }
         }
-        if (this.buttonPressed === 'ArrowRight') {
-            this.swap();
+        if (this.buttonPressed === 'ArrowRight' && swapper) {
+            if (this.isAtExtremity(swapper) === 'Right') {
+                this.endToEnd(swapper);
+            } else {
+                this.swap();
+            }
         }
     }
 
     addId(event: MouseEvent) {
+        this.clearIds(event);
         const current = event.currentTarget as HTMLElement;
         if (current.id === 'swap-selected') {
             current.id = '';
@@ -32,7 +42,16 @@ export class TileHolderComponent {
         }
     }
 
-    swap() {
+    private clearIds(event: MouseEvent) {
+        const current = event.currentTarget as HTMLElement;
+        if (current.parentElement) {
+            for (let i = 0; i < current.parentElement.childElementCount; i++) {
+                current.parentElement.children[i].id = '';
+            }
+        }
+    }
+
+    private swap() {
         const swapper = document.getElementById('swap-selected');
         let prevNode;
         let nextNode;
@@ -42,33 +61,24 @@ export class TileHolderComponent {
         }
         if (this.buttonPressed === 'ArrowLeft' && swapper && prevNode && swapper.parentElement && swapper.parentElement.firstElementChild) {
             swapper.parentElement.insertBefore(swapper, prevNode);
-            /* if (swapper.parentElement.firstElementChild.isEqualNode(swapper) && swapper.parentElement.lastElementChild) {
-                swapper.parentElement.insertBefore(swapper, swapper.parentElement.lastElementChild.nextElementSibling);
-            } */
         } else if (this.buttonPressed === 'ArrowRight' && nextNode && swapper && swapper.parentElement) {
             swapper.parentElement.insertBefore(swapper, nextNode.nextElementSibling);
-            /* if (swapper.parentElement.firstElementChild?.previousElementSibling === swapper && swapper.parentElement.lastElementChild) {
-                swapper.parentElement.insertBefore(swapper, swapper.parentElement.lastElementChild.nextElementSibling);
-            } */
         }
     }
-    /* swap(elementPosition: number) {
-        const tile = document.getElementById('tile-holder');
-        let current;
-        let nextNode;
-        let prevNode;
-        if (tile) {
-            current = tile.children[elementPosition];
-            nextNode = tile.children[elementPosition].nextElementSibling;
-            prevNode = tile.children[elementPosition].previousElementSibling;
+
+    private endToEnd(swapper: HTMLElement) {
+        if (this.buttonPressed === 'ArrowLeft' && swapper && swapper.parentElement && swapper.parentElement.lastElementChild) {
+            swapper.parentElement.insertBefore(swapper, swapper.parentElement.lastElementChild.nextElementSibling);
+        } else if (this.buttonPressed === 'ArrowRight' && swapper && swapper.parentElement && swapper.parentElement.firstElementChild) {
+            swapper.parentElement.insertBefore(swapper, swapper.parentElement.firstElementChild);
         }
-        console.log(tile);
-        console.log(elementPosition);
-        if (this.buttonPressed === 'ArrowLeft' && current && prevNode && tile) {
-            tile.insertBefore(current, prevNode);
-            elementPosition -= 1;
-        } else if (this.buttonPressed === 'ArrowRight' && nextNode && tile) {
-            tile.insertBefore(tile.children[elementPosition], nextNode.nextElementSibling);
+    }
+
+    private isAtExtremity(node: HTMLElement) {
+        if (node.parentElement && node.parentElement.lastElementChild) {
+            if (node.isEqualNode(node.parentElement.firstElementChild)) return 'Left';
+            if (node.isEqualNode(node.parentElement.lastElementChild)) return 'Right';
         }
-    } */
+        return '';
+    }
 }
