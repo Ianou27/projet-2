f4f8d7bb6dd4f89dec69afcaa308632c19570439
RichardT15 <richard.tham@polymtl.ca>
2022-02-22 16:55:42 -0500

    RT:added test for reserveLetters

--- server/app/classes/reserveLetters/reserve-letters.spec.ts
+++ server/app/classes/reserveLetters/reserve-letters.spec.ts
@@ -1,5 +1,18 @@
+import { expect } from 'chai';
+import { ReserveLetters } from './reserve-letters';
+
 describe('ReserveLetters', () => {
+    let reserve: ReserveLetters;
+
+    beforeEach(() => {
+        reserve = new ReserveLetters();
+    });
+
     it('should create an instance', () => {
-        // expect(new ReserveLetters()).toBeTruthy();
+        expect(typeof reserve).to.equal('object');
+    });
+
+    it('should create a reserve of 102 letters', () => {
+        expect(reserve.letters.length).to.equal(102);
     });
 });
