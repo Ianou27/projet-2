b27a50c1d8487cc472b62dd41f9346ef13eac952
Ian Alexis <you@example.com>
2022-03-02 21:06:36 -0500

    almost done with the letter manipulation on the tile holder

--- client/src/app/components/tile-holder/tile-holder.component.ts
+++ client/src/app/components/tile-holder/tile-holder.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener } from '@angular/core';
 import { TileHolderService } from '@app/services/tile-holder/tile-holder.service';
-import { Tile } from './../../../../../common/tile/Tile';
 
 @Component({
     selector: 'app-tile-holder',
@@ -8,32 +7,37 @@ import { Tile } from './../../../../../common/tile/Tile';
     styleUrls: ['./tile-holder.component.scss'],
 })
 export class TileHolderComponent {
-    tileStyle: string = 'tile';
     buttonPressed = '';
-    tile: Tile;
-    letters: string[];
-    firstOccurrence: string | undefined;
     scrollDirection = 0;
-    parent = '';
+    count = 0;
+    lastKeys: string[] = [];
+    showButtonsBool: boolean = false;
     constructor(public tileHolderService: TileHolderService) {}
 
     @HostListener('body:keydown', ['$event'])
     buttonDetect(event: KeyboardEvent) {
         const swapper = document.getElementById('swap-selected');
         this.buttonPressed = event.key;
-        /* for (this.tile of this.tileHolderService.tileHolder) {
-            if (this.tile.letter === this.buttonPressed.toUpperCase()) {
-                console.log('yes');
-            }
-        } */
         if (this.buttonPressed === 'ArrowLeft' && swapper) {
             this.handleSide('Left', swapper);
         }
         if (this.buttonPressed === 'ArrowRight' && swapper) {
             this.handleSide('Right', swapper);
         }
+        if (this.getLettersOnHolder().includes(this.buttonPressed.toUpperCase())) {
+            const indexes = this.findLetterIndexes(this.buttonPressed.toUpperCase());
+            this.addIdOnKey(indexes);
+        } else if (
+            swapper &&
+            !this.getLettersOnHolder().includes(this.buttonPressed.toUpperCase()) &&
+            !(this.buttonPressed === 'ArrowLeft') &&
+            !(this.buttonPressed === 'ArrowRight')
+        ) {
+            this.count = 0;
+            this.clearAllIds(swapper);
+        }
     }
-    @HostListener('body:mousewheel', ['$event'])
+    @HostListener('mousewheel', ['$event'])
     onScroll(event: WheelEvent) {
         const swapper = document.getElementById('swap-selected');
         this.scrollDirection = event.deltaY;
@@ -44,44 +48,74 @@ export class TileHolderComponent {
             this.handleSide('Right', swapper);
         }
     }
+    @HostListener('blur', ['$event'])
+    clickOut() {
+        const tile = document.getElementsByTagName('app-tile')[225] as HTMLElement;
+        this.clearAllIds(tile);
+    }
 
     addId(event: MouseEvent) {
         const current = event.currentTarget as HTMLElement;
-        this.clearIds(current);
+        this.clearAllIds(current);
         current.id = 'swap-selected';
         current.children[0].classList.replace('tile', 'selected');
     }
 
-    addSwapId(event: MouseEvent) {
+    addExchangeId(event: MouseEvent) {
         event.preventDefault();
         const current = event.currentTarget as HTMLElement;
+        this.clearSelectedId(current);
+        if (current.id === '') {
+            current.id = 'swap-reserve';
+            current.children[0].classList.replace('tile', 'selected-reserve');
+        } else if (current.id === 'swap-reserve') {
+            current.id = '';
+            current.children[0].classList.replace('selected-reserve', 'tile');
+        }
+        this.showButtonsBool = this.checkForID();
+    }
+
+    clearExchangeId(event: MouseEvent) {
+        const current = event.currentTarget as HTMLElement;
+        if (current.previousElementSibling) {
+            for (let i = 0; i < current.previousElementSibling.childElementCount; i++) {
+                current.previousElementSibling.children[i].id = '';
+                current.previousElementSibling.children[i].children[0].classList.replace('selected-reserve', 'tile');
+            }
+        }
+        this.showButtonsBool = false;
+    }
+
+    private checkForID() {
+        const parent = document.getElementById('tile-holder');
+        if (parent) {
+            for (let i = 0; i < parent.childElementCount; i++) {
+                if (parent.children[i].id === 'swap-reserve') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private clearAllIds(current: HTMLElement) {
+        if (current.parentElement) {
+            for (let i = 0; i < current.parentElement.childElementCount; i++) {
+                current.parentElement.children[i].id = '';
+                current.parentElement.children[i].children[0].classList.replace('selected', 'tile');
+                current.parentElement.children[i].children[0].classList.replace('selected-reserve', 'tile');
+            }
+        }
+        this.showButtonsBool = this.checkForID();
+    }
+
+    private clearSelectedId(current: HTMLElement) {
         if (current.parentElement) {
             for (let i = 0; i < current.parentElement.childElementCount; i++) {
                 if (current.parentElement.children[i].id === 'swap-selected') {
                     current.parentElement.children[i].id = '';
                     current.parentElement.children[i].children[0].classList.replace('selected', 'tile');
                 }
-            }
-        }
-        current.id = 'swap-reserve';
-        current.children[0].classList.replace('tile', 'selected-reserve');
-    }
-
-    clearSwapReserve(event: MouseEvent) {
-        const current = event.currentTarget as HTMLElement;
-        if (current.previousElementSibling) {
-            for (let i = 0; i < current.previousElementSibling.childElementCount; i++) {
-                current.previousElementSibling.children[i].children[0].classList.replace('selected-reserve', 'tile');
-            }
-        }
-    }
-
-    private clearIds(current: HTMLElement) {
-        if (current.parentElement) {
-            for (let i = 0; i < current.parentElement.childElementCount; i++) {
-                current.parentElement.children[i].id = '';
-                current.parentElement.children[i].children[0].classList.replace('selected', 'tile');
-                current.parentElement.children[i].children[0].classList.replace('selected-reserve', 'tile');
             }
         }
     }
@@ -133,5 +167,61 @@ export class TileHolderComponent {
             if (node.isEqualNode(node.parentElement.lastElementChild)) return 'Right';
         }
         return '';
+    }
+
+    private getLettersOnHolder() {
+        const letterArray = [];
+        const tileHolder = document.getElementById('tile-holder');
+        if (tileHolder) {
+            for (let i = 0; i < tileHolder.childElementCount; i++) {
+                if (tileHolder.children[i].children[0].children[0]) {
+                    letterArray.push(tileHolder.children[i].children[0].children[0].textContent);
+                }
+            }
+        }
+        return letterArray;
+    }
+
+    private findLetterIndexes(letter: string) {
+        const letterArray = this.getLettersOnHolder();
+        const indexes = letterArray
+            .map((x, index) => {
+                if (x === letter) {
+                    return index;
+                }
+                return -1;
+            })
+            .filter((x) => x !== -1);
+        return indexes;
+    }
+
+    private addIdOnKey(indexes: number[]) {
+        const parent = document.getElementById('tile-holder');
+        const lastKey = this.getLastKey();
+        let current;
+        if (indexes.length === 1 && parent) {
+            current = parent.children[indexes[0]] as HTMLElement;
+        } else {
+            if (parent && this.count < indexes.length && lastKey === this.buttonPressed) {
+                current = parent.children[indexes[this.count]] as HTMLElement;
+            } else if (parent && (this.count >= indexes.length - 1 || lastKey !== this.buttonPressed)) {
+                this.count = 0;
+                current = parent.children[indexes[this.count]] as HTMLElement;
+            }
+            this.count++;
+        }
+        if (current) {
+            this.clearAllIds(current);
+            current.id = 'swap-selected';
+            current.children[0].classList.replace('tile', 'selected');
+        }
+    }
+
+    private getLastKey() {
+        this.lastKeys.push(this.buttonPressed);
+        if (this.lastKeys.length > 2) {
+            this.lastKeys.shift();
+        }
+        return this.lastKeys[0];
     }
 }
