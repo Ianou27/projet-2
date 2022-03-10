f94cf9b9bc2e5fe7dc20d957a9972a9a400cacff
Blinkod <71103018+Blinkod@users.noreply.github.com>
2022-03-10 14:18:42 -0500

    Tests pour clueCommand

--- server/app/classes/clueCommand/clue-command.spec.ts
+++ server/app/classes/clueCommand/clue-command.spec.ts
@@ -1,1 +1,63 @@
-describe('ClueCommand', () => {});
+import { CaseProperty } from '@common/assets/case-property';
+import { letterValue } from '@common/assets/reserve-letters';
+import { Tile } from '@common/tile/Tile';
+import { assert, expect } from 'chai';
+import * as sinon from 'sinon';
+import { Game } from './../game/game';
+import { PlacementCommand } from './../placementCommand/placement-command';
+import { Player } from './../player/player';
+import { VirtualPlayer } from './../virtualPlayer/virtual-player';
+import { ClueCommand } from './clue-command';
+
+describe('ClueCommand', () => {
+    let game: Game = new Game();
+    let lettersTilePlayer: Tile[] = [];
+    const lettersPlayer = ['A', 'R', 'A', 'B', 'R', 'E', '*'];
+
+    beforeEach(() => {
+        game = new Game();
+
+        for (const letterPlayer of lettersPlayer) {
+            const tile1: Tile = new Tile(CaseProperty.Normal, 0, 0);
+            tile1.letter = letterPlayer;
+            tile1.value = letterValue[letterPlayer];
+            lettersTilePlayer.push(tile1);
+        }
+
+        game.player1 = new Player(lettersTilePlayer, true, 'player1', { username: 'rt', id: '1', room: 'room1' });
+        game.player2 = new Player(lettersTilePlayer, true, 'player2', { username: 'aa', id: '2', room: 'room1' });
+    });
+
+    afterEach(() => {
+        lettersTilePlayer = [];
+    });
+
+    it('method verifyFormat should return true if the format is valid', () => {
+        const validClueCommand = '!indice';
+        const result = ClueCommand.verifyFormat(validClueCommand.split(' '));
+        expect(result).to.equal(true);
+    });
+
+    it('method verifyFormat should return false if the format is not valid', () => {
+        const validClueCommand = '!indicee';
+        const result = ClueCommand.verifyFormat(validClueCommand.split(' '));
+        expect(result).to.equal(false);
+    });
+
+    it('method findClues should call findAllPlacementCommands from VirtualPlayer', () => {
+        const spy = sinon.stub(VirtualPlayer, 'findAllPlacementCommands');
+        ClueCommand.findClues(game);
+        assert(spy.call);
+    });
+
+    it('method findClues should return 3 placements on the first placement', () => {
+        const placements = ClueCommand.findClues(game);
+        expect(placements.length).to.equal(4);
+    });
+
+    it('method findClues should return 3 placements on the second placement', () => {
+        PlacementCommand.placeWord('!placer h8v arbre'.split(' '), game);
+        const placements = ClueCommand.findClues(game);
+        expect(placements.length).to.equal(4);
+    });
+});
