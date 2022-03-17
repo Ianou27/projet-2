d1e8bb993e0f10961f4faca39de5824f4ffe2e7b
RichardT15 <richard.tham@polymtl.ca>
2022-03-16 22:23:26 -0400

    RT: test of key handler

--- client/src/app/components/board/board.component.spec.ts
+++ client/src/app/components/board/board.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChatService } from '@app/services/chat.service';
@@ -22,5 +23,59 @@ describe('BoardComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('method keyHandler should call clearAll method when event key is Escape', () => {
+        const expectedKey = 'Escape';
+        const buttonEvent = {
+            key: expectedKey,
+        } as KeyboardEvent;
+        const spy = spyOn<any>(component, 'clearAll').and.stub();
+        component.keyHandler(buttonEvent);
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('method keyHandler should call clearAll method when event key is Enter', () => {
+        const expectedKey = 'Enter';
+        const buttonEvent = {
+            key: expectedKey,
+        } as KeyboardEvent;
+        const spy = spyOn<any>(component, 'placeWord').and.stub();
+        component.keyHandler(buttonEvent);
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('method keyHandler should call removeLetter method when event key is Backspace', () => {
+        const expectedKey = 'Backspace';
+        const buttonEvent = {
+            key: expectedKey,
+        } as KeyboardEvent;
+        const spy = spyOn<any>(component, 'removeLetter').and.stub();
+        component.keyHandler(buttonEvent);
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('method keyHandler should call clearAll method when event key is alphabet', () => {
+        const expectedKey = 'a';
+        const buttonEvent = {
+            key: expectedKey,
+        } as KeyboardEvent;
+        const spy = spyOn<any>(component, 'placeLetter').and.stub();
+        component.keyHandler(buttonEvent);
+
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('method isUpper should return true is a letter is upper case', () => {
+        const key = 'A';
+        expect(component.isUpper(key)).toBeTrue();
+    });
+
+    it('method isUpper should return false is a letter is lower case', () => {
+        const key = 'a';
+        expect(component.isUpper(key)).toBeFalse();
     });
 });
