5e815e3f8c1ccdbf999f61d59cf51a88d832f540
David Ringuet <david.ringuet@polymtl.ca>
2022-03-24 14:33:33 -0400

    DR aide message

--- server/app/classes/help-command/help-command.ts
+++ server/app/classes/help-command/help-command.ts
@@ -1,24 +1,22 @@
-import { LetterScore } from './../../../../common/assets/reserve-letters';
 export class HelpCommand {
     static verifyFormat(commandInformations: string[]): boolean {
         if (commandInformations.length !== 1) return false;
         const commandFormat = /^!aide$/;
         return commandFormat.test(commandInformations[0]);
     }
-    // messageAide = "Commandes disponibles :
-    // !aide: Permet d'afficher ce dialogue
-    // !placer : Permet de placer un mot aux coordonnées spécifiées
-    // !réserve: Affiche la quantité restante de chaque lettre dans la réserve
-    // !échanger : Permet d'échanger des lettres de votre chevalet pour des lettres pigées aléatoirement";
-    static help(reserveLetters: string[]): LetterScore {
-        const currentReserve: LetterScore = {};
-        reserveLetters.forEach((letter) => {
-            if (currentReserve[letter] === undefined) {
-                currentReserve[letter] = 1;
-            } else {
-                currentReserve[letter] = currentReserve[letter] + 1;
-            }
-        });
-        return currentReserve;
+    static help(/*reserveLetters: string[]): LetterScore*/) {
+        const messageAide = 'Commandes disponibles : !aide: Permet d`afficher ce dialogue'
+        '!placer : Permet de placer un mot aux coordonnées spécifiées'
+        '!réserve: Affiche la quantité restante de chaque lettre dans la réserve
+        '!échanger : Permet d`échanger des lettres de votre chevalet pour des lettres pigées aléatoirement';
+        // const currentReserve: LetterScore = {};
+        // reserveLetters.forEach((letter) => {
+        //     if (currentReserve[letter] === undefined) {
+        //         currentReserve[letter] = 1;
+        //     } else {
+        //         currentReserve[letter] = currentReserve[letter] + 1;
+        //     }
+        // });
+        // return messageAide;
     }
 }
