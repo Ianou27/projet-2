b4561689ab5ea31f3055e6919bb86e76c25f2284
RichardT15 <richard.tham@polymtl.ca>
2022-03-31 09:48:19 -0400

    RT: added tripleE verification

---
+++ server/app/classes/goal/goal.ts
@@ -0,0 +1,17 @@
+import { Tile } from '@common/tile/Tile';
+
+export class Goal {
+    static tripleE(words: Tile[][]): boolean {
+        let eCount = 0;
+        for (const word of words) {
+            for (const tile of word) {
+                if (tile.letter.toUpperCase() === 'E') {
+                    eCount++;
+                }
+                if (eCount === 3) return true;
+            }
+            eCount = 0;
+        }
+        return false;
+    }
+}
