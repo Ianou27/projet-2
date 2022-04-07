77457c0d8a6189eb66db0bbf1f42a38a195da90c
ianou27 <alexis.ian23@gmail.com>
2022-04-07 14:43:31 -0400

    refacing for main page

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BestScoreComponent } from '@app/pages/best-score/best-score.component';
 
@@ -7,11 +7,27 @@ import { BestScoreComponent } from '@app/pages/best-score/best-score.component';
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements AfterViewChecked {
     animal: string;
     name: string;
 
     constructor(public dialog: MatDialog) {}
+
+    ngAfterViewChecked(): void {
+        const title = document.getElementsByClassName('rectangle')[0];
+        if (title) {
+            for (let i = 0; i < title.childElementCount; i++) {
+                title.children[i].children[0].classList.replace('tile', 'tileTitle');
+                title.children[i].children[0].children[0].id = 'letterTitle';
+                const letter = title.children[i].children[0].children[0] as HTMLElement;
+                letter.style.fontSize = '40px';
+                const score = title.children[i].children[0].children[1] as HTMLElement;
+                score.style.fontSize = '20px';
+                /* title.children[i].children[0].children[0].classList.add('letter');
+                title.children[i].children[0].children[1].classList.add('score'); */
+            }
+        }
+    }
 
     openDialog(): void {
         this.dialog.open(BestScoreComponent, {
