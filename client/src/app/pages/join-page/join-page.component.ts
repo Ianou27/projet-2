2bc89e17f802f2e41d1b1d92b3b07b4c14f4da52
David Ringuet <david.ringuet@polymtl.ca>
2022-04-07 14:07:44 -0400

    DR description dictionary joinpage

--- client/src/app/pages/join-page/join-page.component.ts
+++ client/src/app/pages/join-page/join-page.component.ts
@@ -39,13 +39,19 @@ export class JoinPageComponent implements OnInit {
         { value: '300', text: '5:00' },
     ];
 
-    // dictionaries: Dic[] = this.socketHandler.dictInfoList;
-    // [
-    //     // { value: '1', text: 'Dictionnaire par defaut' },
-    //     { value: '2', text: 'Dictionnaire 2...' },
-    //     { value: '3', text: 'Dictionnaire 3...' },
-    //     { value: '4', text: 'Dictionnaire 4...' },
-    // ];
+    // À enlever et changer dans le .ts
+    dictionaries: Dic[] = this.socketHandler.dictInfoList;
+    dictionariess = [
+        // { value: '1', text: 'Dictionnaire par defaut' },
+        { value: '2', text: 'Dictionnaire 2 wow', description: 'Exemple de descrition du deuxième dictionnaire' },
+        {
+            value: '3',
+            text: 'Dictionnaire mandarin',
+            description: 'Tout ca n`est qu`un exemple à enlever après le vrai lien entre les dictionnaires',
+        },
+        { value: '4', text: 'Dictionnaire compliqué', description: 'smoke okok yeyeyeyeyey hahahah jadore les dictionnaires' },
+    ];
+    // Jusqu'ici
 
     botType = [{ value: BotType.Beginner }, { value: BotType.Expert }];
 
