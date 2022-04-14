930e2ce49fe01167a4ba54f174a1e625641893cf
ianou27 <alexis.ian23@gmail.com>
2022-04-14 09:14:01 -0400

    added tests for grid service

--- client/src/app/services/grid/grid.service.spec.ts
+++ client/src/app/services/grid/grid.service.spec.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* eslint-disable dot-notation */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { TestBed } from '@angular/core/testing';
-import { WORD_2X } from '@app/../../../common/constants/tile-information';
+import { LETTER_3X, WORD_2X } from '@app/../../../common/constants/tile-information';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { GridService } from '@app/services/grid/grid.service';
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../../../../common/constants/board';
 
 describe('GridService', () => {
     let service: GridService;
@@ -31,15 +34,25 @@ describe('GridService', () => {
         expect(service.width).toEqual(CANVAS_HEIGHT);
     });
 
+    it(' clear should call clearRect on grid context', () => {
+        const clearSpy = spyOn<any>(service.gridContext, 'clearRect');
+        service['clear']();
+        expect(clearSpy).toHaveBeenCalled();
+        expect(clearSpy).toHaveBeenCalledWith(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    });
+
+    it(' drawTiles should call drawTilesColor', () => {
+        const drawTilesColorSpy = spyOn<any>(service, 'drawTilesColor');
+        service['drawTiles']();
+        expect(drawTilesColorSpy).toHaveBeenCalled();
+        expect(drawTilesColorSpy).toHaveBeenCalledTimes(5);
+    });
+
     it(' draw should call other methods', () => {
         const gridSpy = spyOn<any>(service, 'drawGrid');
-
         const clearSpy = spyOn<any>(service, 'clear');
-
         const drawTilesSpy = spyOn<any>(service, 'drawTiles');
-
         const drawStarSpy = spyOn<any>(service, 'drawStar');
-
         const bonusTypeSpy = spyOn<any>(service, 'writeBonusTypes');
         service.draw(fontSize);
         expect(clearSpy).toHaveBeenCalled();
@@ -51,15 +64,10 @@ describe('GridService', () => {
 
     it(' draw not should call other methods if fontSize <= 0', () => {
         const gridSpy = spyOn<any>(service, 'drawGrid');
-
         const clearSpy = spyOn<any>(service, 'clear');
-
         const drawTilesSpy = spyOn<any>(service, 'drawTiles');
-
         const drawStarSpy = spyOn<any>(service, 'drawStar');
-
         const bonusTypeSpy = spyOn<any>(service, 'writeBonusTypes');
-        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
         service.draw(-1);
         expect(clearSpy).toHaveBeenCalledTimes(0);
         expect(gridSpy).toHaveBeenCalledTimes(0);
@@ -70,23 +78,20 @@ describe('GridService', () => {
 
     it(' writeBonusTypes should call fillText on the canvas', () => {
         const fillTextSpy = spyOn(service.gridContext, 'fillText');
-        // eslint-disable-next-line dot-notation
         service['writeBonusTypes'](WORD_2X, fontSize);
         expect(fillTextSpy).toHaveBeenCalled();
     });
 
     it(' writeBonusTypes should not call fillText on the canvas if fontSize <= 0', () => {
         const fillTextSpy = spyOn(service.gridContext, 'fillText');
-        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-        service['writeBonusTypes'](WORD_2X, -1); // eslint-disable-line dot-notation
+        service['writeBonusTypes'](WORD_2X, -1);
         expect(fillTextSpy).toHaveBeenCalledTimes(0);
     });
 
     it(' writeBonusTypes should color pixels on the canvas', () => {
         let imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const beforeSize = imageData.filter((x) => x !== 0).length;
-        // eslint-disable-next-line dot-notation
-        service['writeBonusTypes'](WORD_2X, fontSize);
+        service['writeBonusTypes'](LETTER_3X, fontSize);
         imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const afterSize = imageData.filter((x) => x !== 0).length;
         expect(afterSize).toBeGreaterThan(beforeSize);
@@ -94,21 +99,18 @@ describe('GridService', () => {
 
     it(' drawTilesColor should call fillRect on the canvas', () => {
         const fillRectSpy = spyOn(service.gridContext, 'fillRect');
-        // eslint-disable-next-line dot-notation
         service['drawTilesColor']('#FF0000', WORD_2X);
         expect(fillRectSpy).toHaveBeenCalled();
     });
 
     it(' drawTilesColor should not call fillRect if color param is not a hex code', () => {
         const fillRectSpy = spyOn(service.gridContext, 'fillRect');
-        // eslint-disable-next-line dot-notation
         service['drawTilesColor']('test', WORD_2X);
         expect(fillRectSpy).toHaveBeenCalledTimes(0);
     });
 
     it(' drawTilesColor should not call fillRect if color param is empty', () => {
         const fillRectSpy = spyOn(service.gridContext, 'fillRect');
-        // eslint-disable-next-line dot-notation
         service['drawTilesColor']('', WORD_2X);
         expect(fillRectSpy).toHaveBeenCalledTimes(0);
     });
@@ -116,7 +118,6 @@ describe('GridService', () => {
     it(' drawTilesColor should color pixels on the canvas', () => {
         let imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const beforeSize = imageData.filter((x) => x !== 0).length;
-        // eslint-disable-next-line dot-notation
         service['drawTilesColor']('#FF0000', WORD_2X);
         imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const afterSize = imageData.filter((x) => x !== 0).length;
@@ -127,7 +128,6 @@ describe('GridService', () => {
         const expectedCallTimes = 28;
         const moveToSpy = spyOn(service.gridContext, 'moveTo');
         const lineToSpy = spyOn(service.gridContext, 'lineTo');
-        // eslint-disable-next-line dot-notation
         service['drawGrid']();
         expect(moveToSpy).toHaveBeenCalledTimes(expectedCallTimes);
         expect(lineToSpy).toHaveBeenCalledTimes(expectedCallTimes);
@@ -136,7 +136,6 @@ describe('GridService', () => {
     it(' drawGrid should color pixels on the canvas', () => {
         let imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const beforeSize = imageData.filter((x) => x !== 0).length;
-        // eslint-disable-next-line dot-notation
         service['drawGrid']();
         imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const afterSize = imageData.filter((x) => x !== 0).length;
@@ -146,8 +145,7 @@ describe('GridService', () => {
     it(' drawStar should color pixels on the canvas', () => {
         let imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const beforeSize = imageData.filter((x) => x !== 0).length;
-        // eslint-disable-next-line dot-notation
-        service['drawStar'](1, 50); // eslint-disable-line @typescript-eslint/no-magic-numbers
+        service['drawStar'](1, 50);
         imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const afterSize = imageData.filter((x) => x !== 0).length;
         expect(afterSize).toBeGreaterThan(beforeSize);
@@ -156,8 +154,7 @@ describe('GridService', () => {
     it(' drawStar should not color pixels on the canvas if params <= 0', () => {
         let imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const beforeSize = imageData.filter((x) => x !== 0).length;
-        // eslint-disable-next-line dot-notation
-        service['drawStar'](0, -4); // eslint-disable-line @typescript-eslint/no-magic-numbers
+        service['drawStar'](0, -4);
         imageData = service.gridContext.getImageData(0, 0, service.width, service.height).data;
         const afterSize = imageData.filter((x) => x !== 0).length;
         expect(afterSize).toBe(beforeSize);
