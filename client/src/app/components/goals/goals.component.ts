fa1cd5d581fc08da6d8aafa5be94480c67743898
ianou27 <alexis.ian23@gmail.com>
2022-04-14 11:25:42 -0400

    css plus beau pour les objectifs

--- client/src/app/components/goals/goals.component.ts
+++ client/src/app/components/goals/goals.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClientSocketHandler } from '@app/services/client-socket-handler/client-socket-handler.service';
 import { GoalInformations } from './../../../../../common/constants/goal-information';
 import { GoalType } from './../../../../../common/constants/goal-type';
@@ -8,10 +8,14 @@ import { GoalType } from './../../../../../common/constants/goal-type';
     templateUrl: './goals.component.html',
     styleUrls: ['./goals.component.scss'],
 })
-export class GoalsComponent {
+export class GoalsComponent implements OnInit {
     goals: GoalInformations[];
     constructor(public clientSocketHandler: ClientSocketHandler) {
-        this.goals = clientSocketHandler.goals;
+        // this.goals = clientSocketHandler.goals;
+    }
+
+    ngOnInit(): void {
+        this.goals = this.clientSocketHandler.goals;
     }
 
     getType(goal: GoalInformations): string {
