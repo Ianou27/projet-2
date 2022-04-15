c10a9705fd1dcc5e9743b606cde429892c7914f3
Blinkod <71103018+Blinkod@users.noreply.github.com>
2022-04-15 14:09:31 -0400

    Modification ordre d'affichage goal

--- common/constants/goal-information.ts
+++ common/constants/goal-information.ts
@@ -1,8 +1,10 @@
 import { GoalType } from './goal-type';
 export interface GoalInformations {
     name: string;
+    title: string;
     value: number;
     isDone: boolean;
     isInGame: boolean;
     type: GoalType;
+    description: string;
 }
