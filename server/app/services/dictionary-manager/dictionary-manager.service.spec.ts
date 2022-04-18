f5559035b0b83d231f43c56f79385c38ce512b99
riad <riadmefti@hotmail.com>
2022-04-18 03:45:58 -0400

    test dic manager

--- server/app/services/dictionary-manager/dictionary-manager.service.spec.ts
+++ server/app/services/dictionary-manager/dictionary-manager.service.spec.ts
@@ -1,117 +1,99 @@
-// /* eslint-disable @typescript-eslint/no-magic-numbers */
-// import { Game } from '@app/classes/game/game';
-// import { Player } from '@app/classes/player/player';
-// import { IdManager } from '@app/services/id-manager/id-manager.service';
-// import { Room } from '@common/types';
-// import { expect } from 'chai';
-// import * as sinon from 'sinon';
-// import * as io from 'socket.io';
-// import { Timer } from './timer-manager.service';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as io from 'socket.io';
+import { DictionaryManager } from "./dictionary-manager.service";
+import * as fs from 'fs';
+import { Dic } from '@common/types';
 
-// describe('Timer Manager tests', () => {
-//     const timer = new Timer('60');
-//     const idManager = new IdManager();
-//     const game = new Game();
-//     const sio = new io.Server();
-//     // const gameManager = new GameManager();
 
-//     beforeEach(() => {
-//         timer.timeLeft = 60;
-//     });
 
-//     afterEach(() => {
-//         idManager.users.forEach(() => {
-//             idManager.users.pop();
-//         });
-//         idManager.rooms.forEach(() => {
-//             idManager.rooms.pop();
-//         });
-//         sinon.restore();
-//     });
+describe('Dictionary Manager Test', () => {
+    const dictionaryManager = new DictionaryManager();
+    const sio = new io.Server();
+    const socketID ='test';
+    const dictionary:Dic = {
+        title: 'test',
+        description: 'test',
+    };
+  
+    beforeEach(() => {
+        sinon.replace(dictionaryManager.databaseService, 'start', async () => {return  null});
+        sinon.replace(dictionaryManager.databaseService, 'closeConnection', async() => {});
+        sinon.replace(dictionaryManager.databaseService, 'getDictionaryInfo', async() => {return []});
+        sinon.replace(dictionaryManager.databaseService, 'getGameHistory', async() => {return []});
+        sinon.replace(dictionaryManager.databaseService, 'getVirtualPlayers', async() => {return []});
+        sinon.replace(dictionaryManager.databaseService, 'insertDictionary', async() => {});
+        sinon.replace(dictionaryManager.databaseService, 'deleteDictionary', async() => {});
+        sinon.replace(dictionaryManager.databaseService, 'modifyDictionary', async() => {});
+        sinon.replace(dictionaryManager.databaseService, 'resetDictionary', async() => {});
+    });
 
-//     it('start should do nothing if time is -1', (done) => {
-//         timer.timeLeft = -1;
-//         timer.start(game, sio);
-//         // eslint-disable-next-line @typescript-eslint/no-empty-function
-//         setTimeout(() => {
-//             done();
-//         }, 1200);
-//     });
+    afterEach(() => {
+        if (fs.existsSync('./assets/dictionaries/test.json')) {
+           fs.unlinkSync('./assets/dictionaries/test.json');
+        }
+        sinon.restore();
+    });
+    it('should upload a dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        
+        expect( fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.true;
+       
+    });
 
-//     it('start should start counting down and emit the time', (done) => {
-//         const user1 = {
-//             username: 'player1',
-//             id: 'a',
-//             room: 'room1',
-//         };
-//         const user2 = {
-//             username: 'player2',
-//             id: 'b',
-//             room: 'room1',
-//         };
-//         game.player1 = new Player(game.reserveLetters.randomLettersInitialization(), true, 'player1', user1);
-//         game.player2 = new Player(game.reserveLetters.randomLettersInitialization(), false, 'player2', user2);
-//         const room: Room = {
-//             player1: 'player1',
-//             player2: 'player2',
-//             time: '60',
-//         };
-//         idManager.users.push(user1);
-//         idManager.users.push(user2);
-//         idManager.rooms.push(room);
-//         timer.timeLeft = 1;
-//         // eslint-disable-next-line @typescript-eslint/no-empty-function
-//         sinon.replace(game, 'passTurn', () => {});
-//         timer.start(game, sio);
-//         setTimeout(() => {
-//             expect(timer.timeLeft).to.equal(60);
-//             done();
-//         }, 1000);
-//     });
+    it('should delete a dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        await dictionaryManager.deleteDictionary(dictionary.title, sio, socketID);
+        expect( fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
+       
+    });
 
-//     it('start should start counting down and emit the time', (done) => {
-//         const user1 = {
-//             username: 'player1',
-//             id: 'a',
-//             room: 'room1',
-//         };
-//         const user2 = {
-//             username: 'player2',
-//             id: 'b',
-//             room: 'room1',
-//         };
-//         game.player1 = new Player(game.reserveLetters.randomLettersInitialization(), true, 'player1', user1);
-//         game.player2 = new Player(game.reserveLetters.randomLettersInitialization(), false, 'player2', user2);
-//         const room: Room = {
-//             player1: 'player1',
-//             player2: 'player2',
-//             time: '60',
-//         };
-//         idManager.users.push(user1);
-//         idManager.users.push(user2);
-//         idManager.rooms.push(room);
-//         // eslint-disable-next-line @typescript-eslint/no-empty-function
-//         sinon.replace(game, 'passTurn', () => {});
-//         timer.start(game, sio);
-//         setTimeout(() => {
-//             expect(timer.timeLeft).to.equal(59);
-//             done();
-//         }, 975);
-//     });
+    it('should delete all dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        await dictionaryManager.deleteAllDictionaries();
+        expect( fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
+       
+    });
 
-//     it('start reset the timer when it goes below -1', () => {
-//         timer.timeLeft = -2;
-//         timer.reset();
-//         expect(timer.timeLeft).to.equal(timer.timerMax);
-//     });
+    it('should modify  dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        await dictionaryManager.modifyDictionary( 'test', 'test', 'test', sio, socketID);
+        
+        expect( fs.existsSync('./assets/dictionaries/' + 'test' + '.json')).to.be.false;
 
-//     it('reset should set time left to 60', () => {
-//         timer.reset();
-//         expect(timer.timeLeft).to.equal(60);
-//     });
+       
+    });
 
-//     it('stop should set time left to 60', () => {
-//         timer.stop();
-//         expect(timer.timeLeft).to.equal(-1);
-//     });
-// });
+    it('should download  dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        const dic =dictionaryManager.downloadDictionary(dictionary.title);
+        
+
+        expect( dic).to.equal('');
+
+       
+    });
+    it('should resetAll  dictionary', async () => {
+        
+        
+        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
+        await dictionaryManager.resetDictionary( sio, socketID);
+        expect( fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
+
+       
+    });
+
+    
+    
+    
+});
