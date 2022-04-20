c34edd832563ff8fde3579dbdf857208d4085e7b
riad <riadmefti@hotmail.com>
2022-04-19 21:04:55 -0400

    100% on dicManager

--- server/app/services/dictionary-manager/dictionary-manager.service.spec.ts
+++ server/app/services/dictionary-manager/dictionary-manager.service.spec.ts
@@ -38,9 +38,7 @@ describe('DictionaryManager', () => {
     });
 
     afterEach(() => {
-        if (fs.existsSync('./assets/dictionaries/test.json')) {
-            fs.unlinkSync('./assets/dictionaries/test.json');
-        }
+       
         sinon.restore();
     });
     it('should upload a dictionary', async () => {
@@ -48,34 +46,30 @@ describe('DictionaryManager', () => {
 
         expect(fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.true;
     });
+    it('should download  dictionary', async () => {
+        const dic = dictionaryManager.downloadDictionary(dictionary.title);
 
+        expect(dic).to.equal('{"title":"test","description":"test"}');
+    });
+
+    it('should modify  dictionary', async () => {
+        await dictionaryManager.modifyDictionary('test', 'nouveau', 'test', sio, socketID);
+
+        expect(fs.existsSync('./assets/dictionaries/' + 'nouveau' + '.json')).to.be.true;
+    });
     it('should delete a dictionary', async () => {
-        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
         await dictionaryManager.deleteDictionary(dictionary.title, sio, socketID);
         expect(fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
     });
 
     it('should delete all dictionary', async () => {
-        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
         await dictionaryManager.deleteAllDictionaries();
         expect(fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
     });
 
-    it('should modify  dictionary', async () => {
-        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
-        await dictionaryManager.modifyDictionary('test', 'nouveau', 'test', sio, socketID);
 
-        expect(fs.existsSync('./assets/dictionaries/' + 'test' + '.json')).to.be.false;
-    });
-
-    it('should download  dictionary', async () => {
-        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
-        const dic = dictionaryManager.downloadDictionary(dictionary.title);
-
-        expect(dic).to.equal('');
-    });
+  
     it('should resetAll  dictionary', async () => {
-        await dictionaryManager.uploadDictionary(sio, socketID, dictionary);
         await dictionaryManager.resetDictionary(sio, socketID);
         expect(fs.existsSync('./assets/dictionaries/' + dictionary.title + '.json')).to.be.false;
     });
